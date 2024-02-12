1ba57265ecc4f85724640fb0535253a806bec816
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-12 18:48:28 -0500

    Update module_cluster.tf

--- module_cluster/module_cluster.tf
+++ module_cluster/module_cluster.tf
@@ -1,1 +1,11 @@
-
+module "kubernetes-engine" {
+  source  = "terraform-google-modules/kubernetes-engine/google"
+  version = "30.0.0"
+  # insert the 6 required variables here
+  ip_range_pods = 
+  ip_range_services = 
+  name = 
+  network = 
+  project_id = 
+  subnetwork = 
+}
