f74a9fa90e217cf90120315b0060ab06fcae2803
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-12 19:45:17 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "gd-jackstraw"
+    organization = "jaimeh26"
 
     workspaces {
-      name = "gcp-test-examples"
+      name = "gcp_cluster"
     }
   }
 }
